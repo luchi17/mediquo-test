c5d07ebdd01e53c7e1ebe50038a8bc536af2199c
Mariluz Parejo Alcázar <luchi.parejo17@gmail.com>
2022-01-24 11:53:17 +0100

    Add locale files needed to change app language in real time

---
+++ MediquoTest/MediquoTest/Resources/Language/AppLanguage.swift
@@ -0,0 +1,13 @@
+//
+//  AppLanguage.swift
+//  MediquoTest
+//
+//  Created by Mariluz Parejo on 24/1/22.
+//
+
+import Foundation
+
+public enum AppLanguage: String {
+    case castellano = "es"
+    case english = "en"
+}
