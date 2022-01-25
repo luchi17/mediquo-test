9c4cf9d386349c27f660fbd381bc45cf1fe6058d
Mariluz Parejo Alcázar <luchi.parejo17@gmail.com>
2022-01-25 18:41:39 +0100

    Localization file

--- MediquoTest/MediquoTest/Resources/Localization/Localization.swift
+++ MediquoTest/MediquoTest/Resources/Localization/Localization.swift
@@ -24,6 +24,8 @@ import Foundation
     internal static var name: String { return Localization.tr("Localizable","characterDetail.name") }
     /// Apodo
     internal static var nick: String { return Localization.tr("Localizable","characterDetail.nick") }
+    /// Frase
+    internal static var quote: String { return Localization.tr("Localizable","characterDetail.quote") }
     /// Sus frases conocidas
     internal static var quotes: String { return Localization.tr("Localizable","characterDetail.quotes") }
     /// Temporada 
@@ -31,9 +33,27 @@ import Foundation
     /// Temporadas en las que aparece
     internal static var seasons: String { return Localization.tr("Localizable","characterDetail.seasons") }
   }
+  internal enum CharacterList {
+    /// Better Call Saul
+    internal static var betterCallSaul: String { return Localization.tr("Localizable","characterList.betterCallSaul") }
+    /// Breaking Bad
+    internal static var breakingBad: String { return Localization.tr("Localizable","characterList.breakingBad") }
+    /// Cerrar
+    internal static var close: String { return Localization.tr("Localizable","characterList.close") }
+    /// Error
+    internal static var error: String { return Localization.tr("Localizable","characterList.error") }
+    /// La imagen no se ha cargado
+    internal static var errorMessage: String { return Localization.tr("Localizable","characterList.errorMessage") }
+    /// Reintentar
+    internal static var retry: String { return Localization.tr("Localizable","characterList.retry") }
+    /// Error del servidor
+    internal static var serverError: String { return Localization.tr("Localizable","characterList.serverError") }
+  }
   internal enum Language {
-    /// English
+    /// Change to english
     internal static var english: String { return Localization.tr("Localizable","language.english") }
+    /// Cambiar a español
+    internal static var spanish: String { return Localization.tr("Localizable","language.spanish") }
   }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
